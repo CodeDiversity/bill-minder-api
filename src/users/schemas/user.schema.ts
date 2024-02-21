02b454e0e908978c4a3bd79b882182f1c10e0e49
Michael Trew <mtrew2015@gmail.com>
2024-02-20 21:50:26 -0500

    fix duplicate key bug

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,8 +1,8 @@
 import * as mongoose from 'mongoose';
 
 export const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   firstName: String,
   lastName: String,
