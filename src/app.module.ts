f1f3790a3d8e8f9a114b9677daf61842a318ea65
Michael Trew <mtrew2015@gmail.com>
2024-03-10 12:16:34 -0400

    rearrange imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,15 +10,13 @@ import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
-    UsersModule,
-    ScheduleModule.forRoot(),
-    MongooseModule.forRoot(
-      process.env.MONGODB_URI || 'mongodb://localhost/your_db_name',
-    ),
-    AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UsersModule,
+    ScheduleModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    AuthModule,
     BillsModule,
   ],
   controllers: [AppController],
